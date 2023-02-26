95db66cc8500f41e7b69d3b2eaffdeb6e6be472c
Yoon Moh Moh Aung <6531503194@lamduan.mfu.ac.th>
2023-02-26 17:45:42 +0630

    repo created

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,134 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello");
+//{ Driver Code Starts
+import java.util.*;
+
+class Node
+{
+    int data;
+    Node next;
+
+    Node(int d)
+    {
+        data = d;
+        next = null;
     }
 }
+
+class Is_LinkedList_Palindrom
+{
+    Node head;
+    Node tail;
+
+    /* Function to print linked list */
+    void printList(Node head)
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+            System.out.print(temp.data+" ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+
+    /* Inserts a new Node at front of the list. */
+    public void addToTheLast(Node node)
+    {
+
+        if (head == null)
+        {
+            head = node;
+            tail = node;
+        } else
+        {
+            tail.next = node;
+            tail = node;
+        }
+    }
+
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while(t>0)
+        {
+            int n = sc.nextInt();
+            //int k = sc.nextInt();
+            Is_LinkedList_Palindrom llist = new Is_LinkedList_Palindrom();
+            //int n=Integer.parseInt(br.readLine());
+            int a1=sc.nextInt();
+            Node head= new Node(a1);
+            Node tail = head;
+            for (int i = 1; i < n; i++)
+            {
+                int a = sc.nextInt();
+                tail.next = new Node(a);
+                tail = tail.next;
+            }
+
+            Solution g = new Solution();
+            if(g.isPalindrome(head) == true)
+                System.out.println(1);
+            else
+                System.out.println(0);
+            t--;
+        }
+
+    }
+}
+
+
+
+
+// } Driver Code Ends
+
+
+/* Structure of class Node is
+class Node
+{
+	int data;
+	Node next;
+
+	Node(int d)
+	{
+		data = d;
+		next = null;
+	}
+}*/
+
+class Solution
+{
+    //Function to check whether the list is palindrome.
+    boolean isPalindrome(Node head)
+    {
+        //Your code here
+
+        Node second = head;Node h1 = head;
+        while(h1!= null){
+            second = h1;
+            second.next = h1.next;
+            h1 = h1.next;
+
+        }
+
+        Node cur = head;
+
+        Node prev = null ; Node next = null;
+        while(cur!= null){
+            next = cur.next;
+            cur.next = prev;
+            prev  = cur;
+            cur = next;
+        }
+        while(head!=null){
+            if(head!= second){
+                return false;
+            }
+            head = head.next;
+
+        }
+        return true;
+    }
+}
+
