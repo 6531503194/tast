44e2e1ec730ba18491ddd6294854bdee10aeb310
Yoon Moh Moh Aung <6531503194@lamduan.mfu.ac.th>
2023-03-28 20:15:31 +0630

    repo created(node)

---
+++ src/DSA/Stack.java
@@ -0,0 +1,116 @@
+package DSA;//{ Driver Code Starts
+/* package whatever; // don't place package name! */
+import java.util.*;
+class Node
+{
+    int data;
+    Node next;
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+class Remove_Duplicate_From_LL
+{
+    Node head;
+    Node temp;
+    public void addToTheLast(Node node)
+    {
+
+        if (head == null)
+        {
+            head = node;
+            temp = node;
+        }
+        else{
+            temp.next = node;
+            temp = node;
+        }
+    }
+
+    void printList()
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+            System.out.print(temp.data+" ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    /* Drier program to test above functions */
+    public static void main(String args[])
+    {
+
+
+        /* Constructed Linked List is 1->2->3->4->5->6->
+           7->8->8->9->null */
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+
+        while(t>0)
+        {
+            int n = sc.nextInt();
+            Remove_Duplicate_From_LL llist = new Remove_Duplicate_From_LL();
+            int a1=sc.nextInt();
+            Node head= new Node(a1);
+            llist.addToTheLast(head);
+            for (int i = 1; i < n; i++)
+            {
+                int a = sc.nextInt();
+                llist.addToTheLast(new Node(a));
+            }
+            //llist.printList();
+            Solution g = new Solution();
+            llist.head = g.removeDuplicates(llist.head);
+            llist.printList();
+
+            t--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+/* The structure of linked list is the following
+class Node
+{
+    int data;
+    Node next;
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+*/
+
+class Solution
+{
+    //Function to remove duplicates from unsorted linked list.
+    public Node removeDuplicates(Node head)
+    {
+        // Your code here
+
+        Node p = head,q ;
+        for(p = head ; p != null ; p = q){
+            for(q = head ; q != null ; q = q.next){
+                if(p.data == q.next.data){
+                   // q.next = null;
+                    if(q.next.next != null)
+                    q.next = q.next.next;
+
+                }
+            }
+        }
+
+        return head;
+
+
+
+
+
+    }
+}
